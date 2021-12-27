021da848318e69aa80477f52ea86768a558c31d3
sayantikabanik <sayantikabanik122@gmail.com>
2021-12-27 09:58:12 +0530

    partial setup for project

---
+++ setup.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+version_file = open(Path.joinpath(Path.cwd(), "VERSION"))
+version = version_file.read().strip()
+
+setup(
+    author="sayantikabanik",
+    description="Forecasting FP2",
+    include_package_data=True,
+    name="mlops_framework",
+    packages=find_packages(),
+    url="https://github.com/sayantikabnik/FP2",
+    version=version,
+)
+
