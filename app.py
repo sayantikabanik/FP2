15723ade1107340382f663586c1a60c7e50530bf
sayantikabanik <sayantikabanik122@gmail.com>
2022-01-25 20:27:16 +0530

    print added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
-
+print(model, file=sys.stderr)
 
 @app.route('/')
 def home():
@@ -16,6 +16,7 @@ def predict():
     int_features = [ int(x) for x in request.form.values() ]
     final_features = [ np.array(int_features) ]
     prediction = model.predict(final_features)
+    print(prediction, file=sys.stderr)
     output = prediction #round(prediction[0], 2)
     return render_template('index.html', prediction_text='Predicted Price (Monthly Avg - INR) $ {}'.format(output))
 
